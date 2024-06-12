f6c82f7c6e4a17e4470e719a6dfd67ee8ff604f3
Catiana Duarte <catiana.md@gmail.com>
2024-06-12 20:49:08 +0200

    calculating the value of the product

--- js/app.js
+++ js/app.js
@@ -1,9 +1,16 @@
 function add(params) {
     //retrieve value from product name, quantity and value
-let nameProduct = document.getElementById('product');
+let product = document.getElementById('product').value;
+let nameProduct = product.split ('-') [0];
+let unitaryValue = product.split('€')[1]
 let quantity = document.getElementById('quantity');
-alert(nameProduct.value);
+alert(nameProduct);
+alert(unitaryValue);
 alert(quantity.value);
+let price = quantity.value * unitaryValue;
+alert(price);
+
+
 
     //calculate price and subtotal
     //Add to Cart
