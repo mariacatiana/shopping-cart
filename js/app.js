b69d78ef3bf2a74ac53a58533477ca0bb75f32b2
Catiana Duarte <catiana.md@gmail.com>
2024-06-12 20:07:17 +0200

    getting the added product

---
+++ js/app.js
@@ -0,0 +1,14 @@
+function add(params) {
+    //retrieve value from product name, quantity and value
+let nameProduct = document.getElementById('product');
+let quantity = document.getElementById('quantity');
+alert(nameProduct.value);
+alert(quantity.value);
+
+    //calculate price and subtotal
+    //Add to Cart
+    //update the total purchase amount
+}
+function clean(params) {
+    
+}
